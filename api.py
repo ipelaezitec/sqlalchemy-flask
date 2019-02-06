2cccfec0775de2a03b9d9432e078109c45b08fcf
xjfw1 <ipelaez@itecriocuarto.org.ar>
2019-02-05 22:08:45 -0300

    json web token added and admin permissions

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
-from flask import Flask , request, jsonify 
+from flask import Flask , request, jsonify ,make_response
 # from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 import uuid
+import jwt
+# import PyJWT
 from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -24,8 +28,33 @@ class Todo(db.Model):
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer)
 
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return jsonify({'message' : 'Token is missing'}),401
+        try:
+            data= jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 401
+
+        return f(current_user,*args, **kwargs)
+    return decorated
+
+
 @app.route('/user' , methods = ['GET'])
-def get_all_users():
+@token_required
+def get_all_users(current_user):
+
+    if not current_user.admin:
+        return jsonify({'message' : 'Cannot perform that function!'})
+
     users = User.query.all()
     output = []
     for user in users:
@@ -40,7 +69,12 @@ def get_all_users():
     return jsonify({'users' : output})
 
 @app.route('/user/<public_id>', methods = ['GET'])
-def get_one_user(public_id):
+@token_required
+def get_one_user(current_user,public_id):
+
+    if not current_user.admin:
+    return jsonify({'message' : 'Cannot perform that function!'})
+    
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user : 
@@ -57,7 +91,11 @@ def get_one_user(public_id):
 
     
 @app.route('/user', methods=['POST'])
-def create_user():
+@token_required
+def create_user(current_user):
+    if not current_user.admin:
+        return jsonify({'message' : 'Cannot perform that function!'})
+
     data = request.get_json()
 
     hashed_password =  generate_password_hash(data['password'] , method = 'sha256')
@@ -72,7 +110,11 @@ def create_user():
     return jsonify({'message' : 'New user created! '})
 
 @app.route('/user/<public_id>',methods=['PUT'])
-def promote_user(public_id):
+@token_required
+def promote_user(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message' : 'Cannot perform that function!'})
+
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user :
@@ -84,7 +126,11 @@ def promote_user(public_id):
     return jsonify({'message' : 'the user has been promoted!'})
 
 @app.route('/user/<public_id>', methods = ['DELETE'])
-def delete_user(public_id):
+@token_required
+def delete_user(current_user, public_id):
+    if not current_user.admin:
+        return jsonify({'message' : 'Cannot perform that function!'})
+
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user :
@@ -95,7 +141,24 @@ def delete_user(public_id):
 
     return jsonify({'message': 'The user has been deleted!'})
 
+@app.route('/login')
+def login():
+    auth = request.authorization
 
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401,{'WWW-Authenticate' : 'Basic realm="Login Required!"'})
+
+    user = User.query.filter_by(name=auth.username).first()
+
+    if not user :
+        return make_response('Could not verify', 401,{'WWW-Authenticate' : 'Basic realm="Login Required!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow()+ datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401,{'WWW-Authenticate' : 'Basic realm="Login Required!"'})
 
 
 
